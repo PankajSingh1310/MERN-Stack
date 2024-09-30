42c7abc781f5265b0fb077f2e1335ba2f3cbd478
PankajSingh1310 <130424788+PankajSingh1310@users.noreply.github.com>
2024-09-30 22:13:24 +0530

    setup react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+
 const App = () => {
-  return <h1>Frontend Setup</h1>
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
 export default App;
